123b25076dcef2a44ae273c0c0e420b460933b11
¨Alfonso <alfonsobautistay@gmail.com>
2026-09-21 18:28:30 -0600

    Commit Clase (Parte 1)

---
+++ (2)variable.py
@@ -0,0 +1,44 @@
+# Vamos a crear una variable
+a= 123 #Esta es una variable numerica integer
+b= "alumno"
+v = True
+
+"""""
+print(type(a))
+variaable=3.14
+hola= "Hola mundo"
+print(type(variaable))
+print(type(hola))
+print(type(v))
+"""""
+
+"""deudor="Alvin"
+monto_deuda = 500
+pendiente_deuda = False
+saldo_actual= monto_deuda * pendiente_deuda
+print(saldo_actual)"""
+
+"""compra_semana =  0
+compra_lunes = 100
+compra_semana += compra_lunes
+cantidad_compras = 1
+
+compra_martes = 200
+compra_semana += compra_martes
+cantidad_compras += 1
+
+compra_miercoles = 300
+compra_semana += compra_miercoles
+cantidad_compras += 1
+
+compra_jueves = 400
+compra_semana += compra_jueves
+cantidad_compras += 1
+
+compra_viernes = 500
+compra_semana += compra_viernes
+cantidad_compras += 1
+
+print(compra_semana)
+print(cantidad_compras)"""
+
