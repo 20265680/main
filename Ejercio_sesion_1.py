2a8cfce4ce10b744776dfa0f25dd29a0e77c0a5b
¨Alfonso <alfonsobautistay@gmail.com>
2026-09-25 14:48:31 -0600

    xxx

--- Ejercio_sesion_1.py
+++ Ejercio_sesion_1.py
@@ -2,7 +2,15 @@
 equipo = input("Equipo (Laptop o Tablet): ")
 if equipo == "Laptop":
     horas=int(input("Horas: "))
+    if horas < 0:
+        print("Solo valores positivos")
+    elif horas <= 2:
+        print("Poco")
+    elif horas <= 5:
+        print("Normal")
+    elif horas <= 8:
+        print("Mucho")
+    else: 
+        print("No Aplica")
 else:
-    print("No leyó el programa,tlabaja")
-    
- 
+    print("No leyó el programa,trabaja")
